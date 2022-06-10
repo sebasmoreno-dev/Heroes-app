a2fceb4301622a33bc66236855e5465bd88fe023
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-06-10 14:37:33 -0500

    useMemo on hero data

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useMemo } from "react";
 import getHeroesByPublisher from "./../../selectors/getHeroesByPublisher";
 import HeroCard from "./HeroCard";
 
 
 const HeroList = ({ publisher }) => {
 
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
     <>
--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,4 +1,5 @@
-import { Navigate, useParams } from "react-router-dom";
+import { useMemo } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import getHeroById from "./../../selectors/getHeroById";
 
 
@@ -6,19 +7,57 @@ export const HeroScreen = () => {
 
   const { heroeId } = useParams();
 
-  const hero = getHeroById(heroeId);
+  const hero = useMemo(() => getHeroById(heroeId), [heroeId]);
+
+  const navigate = useNavigate();
+
+  const handleReturn = () => {
+    navigate(-1);
+  }
 
   if (!hero) {
     return <Navigate to='/' />
   }
 
+
+  const {
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+  } = hero;
+
+  const imagePath = `/assets/${id}.jpg`;
+
   return (
-    <>
-      <h1>Hero Screen</h1>
-        <p>
-          {hero.superhero}
-        </p>
-      <div></div>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={imagePath}
+          alt={ superhero }
+          className="img-thumbnail" />
+        </div>
+      
+      <div className="col-8">
+        <h2>{ superhero }</h2>
+        <ul className="list-group-item list-group-flush">
+          <li className="list-group-item"> <b>Alter ego:</b> { alter_ego }</li>
+          <li className="list-group-item"> <b>Publisher:</b> { publisher }</li>
+          <li className="list-group-item"> <b>First appearance:</b> { first_appearance }</li>
+        </ul>
+
+        <h3 className="mt-3">Characters</h3>
+        <p>{ characters }</p>
+
+        <button
+          className="btn btn-outline-info"
+          onClick={ handleReturn }
+        >
+          Regresar
+        </button>
+      </div>
+    </div>
   )
 }
