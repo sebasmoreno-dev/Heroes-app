796cee45e39c7c1a1895bf235f7df5210b26a90e
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-06-11 16:40:15 -0500

    showing conditional messages

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import queryString from 'query-string';
+import { useMemo } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
 import { useForm } from './../../hooks/useForm';
-
+import HeroCard from './../hero/HeroCard';
 
 const SearchScreen = () => {
 
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q =  '' } = queryString.parse(location.search);
+
+
   const [ formValues, handleInputChange ] = useForm({
-    searchText: '',
+    searchText: q,
   });
 
   const { searchText } = formValues;
+  const heroesFileted = useMemo (() => getHeroesByName(q), [q]);
+
+
 
   const handleSearch = (e) => {
     e.preventDefault();
-    console.log(searchText);
+    navigate(`?q=${ searchText }`);
   }
 
 
@@ -46,6 +58,27 @@ const SearchScreen = () => {
             </button>
           </form>
 
+        </div>
+
+        <div className="col-7">
+          <h4>Resultados</h4>
+          <hr />
+
+            {
+              (q === '')
+                ? <div className="alert alert-info">Buscar un héroe</div>
+                : ( heroesFileted.length === 0 )
+                  && <div className="alert alert-danger">No hay resultados: { q } </div>
+
+            }
+
+
+            { heroesFileted.map(hero => (
+              <HeroCard
+                key={ hero.id }
+                { ...hero }
+              />
+            )) }
         </div>
       </div>
     </>
